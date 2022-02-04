6c6c7e0197faf7dce7b8415008ac8a8852052dc6
Maria <mariafleischer111@gmail.com>
2022-02-04 10:17:17 +0100

    [added] baked nav mesh

--- BachelorThese/Assets/Scripts/Non-UI/Companion.cs
+++ BachelorThese/Assets/Scripts/Non-UI/Companion.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Companion : NPC
 {
@@ -15,6 +16,7 @@ public class Companion : NPC
         set
         {
             rigid.velocity = value;
+            //MoveAgentTo();
             MovementDir = value;
             if (value == Vector3.zero)
             {
@@ -32,26 +34,39 @@ public class Companion : NPC
     DialogueManager diaM;
     Rigidbody rigid;
     Animator animator;
+    NavMeshAgent agent;
+
+
     protected override void Start()
     {
         base.Start();
+        SetCompanionValues();
+    }
+    void SetCompanionValues()
+    {
+
         diaM = DialogueManager.instance;
         rigid = GetComponent<Rigidbody>();
         animator = GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
     }
     private void Update()
     {
-        if (inParty)
-        {
-            FollowPlayer();
-        }
+        if (!inParty)
+            return;
+
+        FollowPlayer();
     }
     private void FixedUpdate()
     {
-        if (inParty)
-        {
-            TurnTowardsPlayer((targetPlayer.transform.position - npcMesh.transform.position).normalized);
-        }
+        if (!inParty)
+            return;
+        Vector3 normalizedDirectionToPlayer = (targetPlayer.transform.position - npcMesh.transform.position).normalized;
+        TurnTowardsPlayer(normalizedDirectionToPlayer);
+    }
+    void MoveAgentTo(Vector3 destination)
+    {
+        agent.destination = destination;
     }
     public void JoinParty()
     {
@@ -60,26 +75,32 @@ public class Companion : NPC
     }
     void FollowPlayer()
     {
-        if (!diaM.isInDialogue && !diaM.isOnlyInAsk)
+        if (diaM.isInDialogue || diaM.isOnlyInAsk)
         {
-            Vector3 directionToPlayer = (targetPlayer.transform.position - transform.position).normalized;
-
-            float currentDistance = Vector3.Distance(transform.position, targetPlayer.transform.position);
-            if (currentDistance < minDistanceToPlayer)
-                movementDir = Vector3.zero;
-            else
-                movementDir = directionToPlayer * speed;
-        }
-        else
             movementDir = Vector3.zero;
+            return;
+        }
+
+        Vector3 directionToPlayer = (targetPlayer.transform.position - transform.position).normalized;
+
+        float currentDistance = Vector3.Distance(transform.position, targetPlayer.transform.position);
+        if (currentDistance < minDistanceToPlayer)
+            movementDir = Vector3.zero;
+        else
+            movementDir = directionToPlayer * speed;
+
+    }
+    void GetTargetPositionFromWalkingDirection(Vector3 normalizedDirection)
+    {
+        //transform.position + 
     }
     public override void TurnTowardsPlayer(Vector3 directionToPlayer)
     {
-        if (!diaM.isInDialogue && !diaM.isOnlyInAsk)
-        {
-            directionToPlayer = Vector3.Scale(directionToPlayer, new Vector3(1, 0, 1));
-            npcMesh.transform.forward = Vector3.Lerp(npcMesh.transform.forward, directionToPlayer, 0.4f);
-        }
+        if (diaM.isInDialogue || diaM.isOnlyInAsk)
+            return;
+
+        directionToPlayer = Vector3.Scale(directionToPlayer, new Vector3(1, 0, 1));
+        npcMesh.transform.forward = Vector3.Lerp(npcMesh.transform.forward, directionToPlayer, 0.4f);
     }
     public override void TurnAwayFromPlayer()
     {
