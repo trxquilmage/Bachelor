c099ff964ae397487eb7ecec45d5346b790087b3
Maria <mariafleischer111@gmail.com>
2022-02-07 11:13:59 +0100

    [added] StopWordsLookupReader without being active in scene

--- BachelorThese/Assets/Scripts/Process/WordLookupReader.cs
+++ BachelorThese/Assets/Scripts/Process/WordLookupReader.cs
@@ -7,16 +7,17 @@ using TMPro;
 public class WordLookupReader : MonoBehaviour
 {
     public static WordLookupReader instance;
-    string dataPath, allWords, allLongWords, allQuestions, allTags, allFillers, allBlocked, allQuests;
-    [HideInInspector]
+    string dataPath, allWords, allLongWords, allQuestions, allTags, allFillers, allQuests;
+
     public Dictionary<string, string[]> wordTag = new Dictionary<string, string[]>();
     public Dictionary<string[], string[]> longWordTagSingular = new Dictionary<string[], string[]>();
     public Dictionary<string, string[]> longWordTag = new Dictionary<string, string[]>();
     public Dictionary<string, string[]> questionTag = new Dictionary<string, string[]>();
     public Dictionary<string, string[]> fillerTag = new Dictionary<string, string[]>();
     public Dictionary<string, string> questDescriptions = new Dictionary<string, string>();
-    public List<string> blocked = new List<string>();
     public Dictionary<string, string[]> tagSubtag = new Dictionary<string, string[]>();
+    public List<string> blocked = new List<string>();
+    Searcher searcher;
     List<TMP_WordInfo> currentWordList;
     List<string[]> currentReferenceWords;
     int currentLongWordIndex;
@@ -28,21 +29,23 @@ public class WordLookupReader : MonoBehaviour
     }
     private void Start()
     {
-        refM = ReferenceManager.instance;
-        dataPath = Application.dataPath + "/Data/" + refM.sceneNumber;
+        AssignVariables();
         LookUpWord();
         LookUpLongWord();
         LookUpQuestion();
         LookUpTag();
         LookUpFiller();
-        LookUpBlocked();
         LookUpQuests();
+    }
+    void AssignVariables()
+    {
+        refM = ReferenceManager.instance;
+        dataPath = Application.dataPath + "/Data/" + refM.sceneNumber;
         currentWordList = new List<TMP_WordInfo>();
         currentReferenceWords = new List<string[]>();
+        blocked = StopWordsLookupReader.instance.blockedWords;
+        searcher = new Searcher();
     }
-    /// <summary>
-    /// Go through the excel sheet & save the info
-    /// </summary>
     void LookUpQuestion()
     {
         string pathQuestion = dataPath + "/Questions.csv";
@@ -142,22 +145,6 @@ public class WordLookupReader : MonoBehaviour
             }
         }
     }
-    void LookUpBlocked()
-    {
-        string pathBlocked = dataPath + "/StopWords.txt";
-        allBlocked = File.ReadAllText(pathBlocked);
-        string[] lines = allBlocked.Split("\n"[0]);
-        foreach (string s in lines)
-        {
-            if (s != "")
-            {
-                string line = s.Substring(0, s.Length - 1);
-                line = WordUtilities.CapitalizeAllWordsInString(line);
-                blocked.Add(line);
-            }
-        }
-    }
-
     void LookUpQuests()
     {
         string pathTagLookup = dataPath + "/Quests.csv";
@@ -260,7 +247,7 @@ public class WordLookupReader : MonoBehaviour
                 }
             }
         }
-        
+
         //is this the first word of a longer word chain?
         if (currentWordList.Count == 0)
         {
@@ -316,4 +303,134 @@ public class WordLookupReader : MonoBehaviour
         }
         return isWord;
     }
+}
+public class StopWordsLookupReader : MonoBehaviour
+{
+    public static StopWordsLookupReader instance;
+    public List<string> blockedWords;
+
+    string dataPath;
+    string allBlocked;
+    List<string> blocked;
+    BucketSort bucketSort;
+    void Awake()
+    {
+        instance = this;
+        dataPath = Application.dataPath + "/Data/" + "002";
+
+        LookUpBlocked();
+        StartBucketSort();
+    }
+    void StartBucketSort()
+    {
+        blocked = new List<string>();
+        blockedWords = new List<string>();
+
+        bucketSort = new BucketSort(blocked, 0);
+        blockedWords = bucketSort.GetSortedList();
+    }
+    void LookUpBlocked()
+    {
+        string pathBlocked = dataPath + "/StopWords.txt";
+        allBlocked = File.ReadAllText(pathBlocked);
+        string[] lines = allBlocked.Split("\n"[0]);
+        foreach (string s in lines)
+        {
+            if (s == "")
+                return;
+
+            string line = s.Substring(0, s.Length - 1);
+            line = WordUtilities.CapitalizeAllWordsInString(line);
+            blocked.Add(line);
+        }
+    }
+}
+public class BucketSort
+{
+    int alphabetSize = 26;
+    List<string> listToSort;
+    List<string>[] buckets;
+    List<string> additionalBucket;
+    int currentLetterIndex = 0;
+    public BucketSort(string[] arrayToSort, int currentLetterIndex)
+    {
+        StartBucketSort(ArrayToList(arrayToSort), currentLetterIndex);
+    }
+    public BucketSort(List<string> listToSort, int currentLetterIndex)
+    {
+        StartBucketSort(listToSort, currentLetterIndex);
+    }
+    void StartBucketSort(List<string> listToSort, int currentLetterIndex)
+    {
+        if (currentLetterIndex > 25)
+            return;
+
+        this.listToSort = listToSort;
+        this.currentLetterIndex = currentLetterIndex;
+        buckets = new List<string>[alphabetSize];
+
+        PlaceInBuckets();
+        MergeBuckets();
+        RemoveDuplicates();
+        BucketSort bucketSort = new BucketSort(this.listToSort, currentLetterIndex + 1);
+        this.listToSort = bucketSort.GetSortedList();
+    }
+    List<string> ArrayToList(string[] array)
+    {
+        List<string> list = new List<string>();
+        foreach (string word in array)
+            list.Add(word);
+        return list;
+    }
+    void PlaceInBuckets()
+    {
+        int bucketIndex;
+        foreach (string word in listToSort)
+        {
+            if (currentLetterIndex >= word.Length)
+                return;
+
+            bucketIndex = word[currentLetterIndex] - 'a';
+
+            if (bucketIndex >= alphabetSize)
+            {
+                additionalBucket.Add(word);
+                return;
+            }
+
+            buckets[bucketIndex].Add(word);
+        }
+    }
+    void MergeBuckets()
+    {
+        List<string> allWords = new List<string>();
+        foreach (List<string> bucket in buckets)
+        {
+            for (int i = 0; i < bucket.Count; i++)
+                allWords.Add(bucket[i]);
+        }
+        for (int i = 0; i < additionalBucket.Count; i++)
+            allWords.Add(additionalBucket[i]);
+
+        listToSort = allWords;
+    }
+    void RemoveDuplicates()
+    {
+        List<string> noDuplicates = new List<string>();
+        for (int i = 0; i < listToSort.Count - 1; i++)
+        {
+            if (listToSort[i] != listToSort[i + 1])
+                noDuplicates.Add(listToSort[i]);
+        }
+        listToSort = noDuplicates;
+    }
+    public List<string> GetSortedList()
+    {
+        return listToSort;
+    }
+}
+
+public class Searcher
+{
+
 }
